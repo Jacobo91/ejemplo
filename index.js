a4d7bca2a8eca044eddc5dbb9402837e2138d8d0
Jacobo Velasquez <110640343+Jacobo91@users.noreply.github.com>
2024-04-30 09:22:23 -0500

    update body tag mutation observer template

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 
-const targetNode = document.body;
+function setupMutationObserver() {
+    const targetNode = document.body;
 
-
-const config = { attributes: true, attributeFilter: ['data-nueva-feature'] };
-
-
-const callback = function(mutationsList, observer) {
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'data-nueva-feature') {
-            const newValue = mutation.target.getAttribute('data-nueva-feature');
-            if (newValue === 'true') {
-                console.log('yes');
-                document.querySelector('button').addEventListener('click', () => {
-                    alert('siiiiiii')
-                })
-            } else if (newValue === 'false') {
-                console.log('no');
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'data-carousel-case-color') {
+                const caseColorValue = targetNode.getAttribute('data-carousel-case-color');
+                if (caseColorValue === 'true') {
+                    alert('yes yes');
+                } else if (caseColorValue === 'false') {
+                    alert('no no');
+                }
             }
         }
-    }
-};
+    };
 
+    const observer = new MutationObserver(callback);
 
-const observer = new MutationObserver(callback);
+    const config = { attributes: true, attributeFilter: ['data-carousel-case-color'] };
 
+    observer.observe(targetNode, config);
 
-observer.observe(targetNode, config);
+    // Return the observer in case you need to disconnect it later
+    return observer;
+}
+
+// Call the setup function to start the observer
+setupMutationObserver();
+
 
